fcf9df4ac7e3f266ba45482283590b27c4ec28a1
Deus <147171695+minjaesheep@users.noreply.github.com>
2023-11-24 23:40:22 +0900

    Update files

---
+++ _next/static/m2GYT8-JQelrEtfw6Wi4j/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/86-7e2dac4faa0dc2ec.js","static/chunks/pages/index-fe12c20cdb8f4b84.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
